5149db2f58bc643ec353951cc1dbb16d790fcbcc
Christine Abernathy <ca.abernathy@fb.com>
2012-08-13 18:28:49 -0700

    Initial commit

---
+++ SendRequestsHowTo/SendRequestsHowTo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SendRequestsHowTo
+//
+//  Created by Christine Abernathy on 8/13/12.
+//  Copyright (c) 2012 Facebook Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
